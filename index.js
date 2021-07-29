e6d1c5806a3dca83ebad789ced949c0c3ac46017
Arne Tripolone <arnetripolone@gmail.com>
2021-07-30 09:20:32 +1000

    Readme md file is now generated

--- index.js
+++ index.js
@@ -17,67 +17,79 @@ init();
 const inquirer = require('inquirer');
 const fs = require('fs');
 
-const generateHTML = (answers) =>
-  `<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
-  <title>Document</title>
-</head>
-<body>
-  <div class="jumbotron jumbotron-fluid">
-  <div class="container">
-    <h1 class="display-4">Hi! My name is ${answers.name}</h1>
-    <p class="lead">I am from ${answers.location}.</p>
-    <h3>Example heading <span class="badge badge-secondary">Contact Me</span></h3>
-    <ul class="list-group">
-      <li class="list-group-item">My GitHub username is ${answers.github}</li>
-      <li class="list-group-item">LinkedIn: ${answers.linkedin}</li>
-    </ul>
-  </div>
-</div>
-</body>
-</html>`;
+const generateMD = (answers) =>
+  `
+# ${answers.title}
+
+## Description
+${answers.description}
+
+## Screenshots
+${answers.visuals}
+
+## Installation
+${answers.installation}
+
+## Usage
+${answers.usage}
+
+## Support
+${answers.support}
+
+## Roadmap 
+${answers.roadmap}
+
+## Authors
+${answers.authors}
+`;
 
 inquirer
   .prompt([
     {
       type: 'input',
-      name: 'name',
-      message: 'What is your name?',
+      name: 'title',
+      message: 'What is your project called?',
     },
     {
       type: 'input',
-      name: 'location',
-      message: 'Where are you from?',
+      name: 'description',
+      message: 'Describe your project, particularly its background, context and features:',
     },
     {
       type: 'input',
-      name: 'hobby',
-      message: 'What is your favorite hobby?',
+      name: 'visuals',
+      message: 'Upload any screenshots, GIFS or videos of your project here:',
     },
     {
       type: 'input',
-      name: 'food',
-      message: 'What is your favorite food?',
+      name: 'installation',
+      message: 'List any steps needed to install your project:',
     },
     {
       type: 'input',
-      name: 'github',
-      message: 'Enter your GitHub Username',
+      name: 'usage',
+      message: 'Provide examples of your project being used:',
     },
     {
       type: 'input',
-      name: 'linkedin',
-      message: 'Enter your LinkedIn URL.',
+      name: 'support',
+      message: 'Where can people go if they need support with your project?',
+    },
+    {
+      type: 'input',
+      name: 'roadmap',
+      message: 'How could this project be developed further in the future?',
+    },
+    {
+      type: 'input',
+      name: 'authors',
+      message: 'Who contributed to this project?',
     },
   ])
   .then((answers) => {
-    const htmlPageContent = generateHTML(answers);
+    const mdPageContent = generateMD(answers);
 
-    fs.writeFile('index.html', htmlPageContent, (err) =>
-      err ? console.log(err) : console.log('Successfully created index.html!')
+    fs.writeFile('index.md', mdPageContent, (err) =>
+      err ? console.log(err) : console.log('Successfully created readme.md!')
     );
   });
---
+++ index.md
@@ -0,0 +1,23 @@
+
+# /Users/arnetripolone/Library/Group\ Containers/UBF8T346G9.Office/Outlook/Outlook\ 15\ Profiles/Main\ Profile/Files/S0/1/Attachments/image001\[4299\].png 
+
+## Description
+asfg
+
+## Screenshots
+dg
+
+## Installation
+DZG
+
+## Usage
+dg
+
+## Support
+XDZG
+
+## Roadmap 
+d
+
+## Authors
+XFB
